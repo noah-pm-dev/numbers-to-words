a7f15d27a50f61cbfb341b78e65bd2bd5a2b4106
HuoKnight <noman.gronckle@gmail.com>
2024-09-07 21:06:11 -0700

    move scuffed stuff to expanded script, fix another zero error in both scripts, update readme

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ Similarly, `389027503476340760324867023947863026734067234096723076230763904267` 
 ```
 Three Hundred and Eighty-Nine Vigintillion Twenty-Seven Novemdecillion Five Hundred and Three Octodecillion Four Hundred and Seventy-Six Septendecillion Three Hundred and Forty Sexdecillion Seven Hundred and Sixty Quindecillion Three Hundred and Twenty-Four Quattuordecillion Eight Hundred and Sixty-Seven Tredecillion Twenty-Three Duodecillion Nine Hundred and Forty-Seven Undecillion Eight Hundred and Sixty-Three Decillion Twenty-Six Nonillion Seven Hundred and Thirty-Four Octillion Sixty-Seven Septillion Two Hundred and Thirty-Four Sextillion Ninenty-Six Quintillion Seven Hundred and Twenty-Three Quadrillion Seventy-Six Trillion Two Hundred and Thirty Billion Seven Hundred and Sixty-Three Million Nine Hundred and Four Thousand Two Hundred and Sixty-Seven
 ```
+
+`main.py` can handle numbers into the quinquagintillions. `main_expanded.py` can handle absurdly large numbers, however after centillion it uses a non-standard naming pattern as I don't have a major in googolology. Also the code is a lot more scuffed, so `main.py` is the more concise but less powerful version.
--- main_expanded.py
+++ main_expanded.py
@@ -19,25 +19,36 @@ def convert_two_digits(digits):
             return number_words[digits[0]][1] + "y-" + number_words[digits[1]][0]
 
 
+
 big_number_roots = {
-    11: "Decillion",
-    21: "Vigintillion",
-    31: "Trigintillion",
-    41: "Quadragintillion",
-    51: "Quinquagintillion"
+    11: "llion",
+    101: "Centillion"
 }
 
 big_number_prefixes = {
-    0: '',
-    1: 'Un',
-    2: 'Duo',
-    3: 'Tre',
-    4: 'Quattuor',
-    5: 'Quin',
-    6: 'Sex',
-    7: 'Septen',
-    8: 'Octo',
-    9: 'Novem',
+    "0": "",
+    "1": "Deci",
+    "2": "Viginti",
+    "3": "Triginti",
+    "4": "Quadraginti",
+    "5": "Quinquaginti",
+    "6": "Sexaginti",
+    "7": "Septuaginti",
+    "8": "Octoginti",
+    "9": "Nonaginti"
+}
+
+big_number_prefixes_prefixes = {
+    "0": "",
+    "1": "Un",
+    "2": "Duo",
+    "3": "Tre",
+    "4": "Quattuor",
+    "5": "Quin",
+    "6": "Sex",
+    "7": "Septen",
+    "8": "Octo",
+    "9": "Novem",
 }
 
 big_numbers = {
@@ -54,15 +65,15 @@ big_numbers = {
 }
 
 number_words = {
-    '1': ["One"],
-    '2': ["Two", "Twent"],
-    '3': ["Three", "Thirt"],
-    '4': ["Four", "Fort"],
-    '5': ["Five", "Fift"],
-    '6': ["Six", "Sixt"],
-    '7': ["Seven", "Sevent"],
-    '8': ["Eight", "Eight"],
-    '9': ["Nine", "Ninent"]
+    "1": ["One"],
+    "2": ["Two", "Twent"],
+    "3": ["Three", "Thirt"],
+    "4": ["Four", "Fort"],
+    "5": ["Five", "Fift"],
+    "6": ["Six", "Sixt"],
+    "7": ["Seven", "Sevent"],
+    "8": ["Eight", "Eight"],
+    "9": ["Nine", "Ninent"]
 }
 
 print("Input your number: ")
@@ -72,7 +83,7 @@ split = [num[i:i+3][::-1] for i in range(0, len(num), 3)][::-1]
 
 split_num = split[:]
 for num, i in enumerate(split):
-    if set(i) == {'0'}:
+    if set(i) == {"0"}:
         split_num.pop(0)
     else:
         break
@@ -84,34 +95,34 @@ print(split_num)
 for num, i in enumerate(split_num):
     num_segments -= 1
     
-    if set(i) == {'0'}:
+    if set(i) == {"0"}:
         continue
     if len(i) == 1:
         stdout.write(number_words[i][0])
     elif len(i) == 2:
         stdout.write(convert_two_digits(i))
     elif len(i) == 3:
-        if i[0] != '0':
-            if i[1:] != '00':   
-                stdout.write(number_words[i[0]][0] + ' Hundred and ' + convert_two_digits(i[1:]))
+        if i[0] != "0":
+            if i[1:] != "00":   
+                stdout.write(number_words[i[0]][0] + " Hundred and " + convert_two_digits(i[1:]))
             else:
-                stdout.write(number_words[i[0]][0] + ' Hundred')
-        elif i[1] != '0':
+                stdout.write(number_words[i[0]][0] + " Hundred")
+        elif i[1] != "0":
             if num != 0:
-                stdout.write('and ' + convert_two_digits(i[1:]))
+                stdout.write("and " + convert_two_digits(i[1:]))
             else:
                 stdout.write(convert_two_digits(i[1:]))
-        elif i[2] != '0':
-            stdout.write('and ' + number_words[i[2]][0])
+        elif i[2] != "0":
+            stdout.write("and " + number_words[i[2]][0])
     
     if num_segments > 10:
         for i in big_number_roots:
             try:
-                if num_segments in range(i, i + 10):
-                    stdout.write(' ' + big_number_prefixes[num_segments - i] + big_number_roots[i].lower() + ' ')
+                if num_segments in range(i, i + 90):
+                    stdout.write(" " + big_number_prefixes_prefixes[str(num_segments - 1)[-2:][1:]] + big_number_prefixes[str(num_segments - 1)[-2:][:1]] + big_number_roots[i] + " ")
                     continue
             except KeyError:
-                print('\033[33mwtf\033[0m')
+                print("\033[33mwtf\033[0m")
     else:
         try:
             stdout.write(" " + big_numbers[num_segments] + " ")
